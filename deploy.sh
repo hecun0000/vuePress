84c786df84f6def6c7313c7b02aa0eef045c5251
hecun <16395864338@qq.com>
2019-09-15 09:26:38 +0800

    @deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #docker build -t kkb-frontend git@github.com:kkbjs/vue-docker.git
 # dist包在git李
+echo '执行deploy.sh'
 git pull
+echo 'git pull'
+npm run docs:build
+echo "build"
 docker build -t vue-presss-blog .
 docker stop vue-presss-blog-container
 docker rm vue-presss-blog-container
