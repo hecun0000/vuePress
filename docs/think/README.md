7fbc43b5b1cb7342a474b6934a7bf4f58256f05c
hecun <16395864338@qq.com>
2019-09-15 15:35:55 +0800

    @deploy 删除console

--- docs/think/README.md
+++ docs/think/README.md
@@ -1,9 +1,3 @@
 # 随想
 
 #### [关于理财](/think/关于理财.md)
-
-测试 webhookds  
-
-
-
-#   deploy 
--- webhooks.js
+++ webhooks.js
@@ -6,31 +6,20 @@ const app = express()
 const childProcess = require('child_process');
 const worker = childProcess.fork('./wechat.js')
 
-// setInterval(_=>{
-//   worker.send({
-//     author: 'ddd',
-//     msg: 'hhhhhh'
-//   })
-// }, 3000)
-
 app.use(bodyParser.json())
 
 function run_cmd(cmd, args, cb) {
   var spawn = require('child_process').spawn
   var child = spawn(cmd, args)
   var resq = ''
-
   child.stdout.on("data", (buffer) => {resq += buffer.toString();})
   child.stdout.on("end", () => {resq += cb(resq)})
 }
-
-
 
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-console.log(worker)
 app.post('/webhooks', function (req, res) {
   const SECRET_TOKEN = 'jy211985';
   const signature = `sha1=${crypto
@@ -44,11 +33,9 @@ app.post('/webhooks', function (req, res) {
       console.log('push master')
       let msg = req.body.head_commit.message
       let author = req.body.head_commit.author.name
-      console.log(msg, author)
       if(msg.indexOf('@deploy')>-1) {
         console.log('deploy master')
         run_cmd('sh', ['./deploy.sh'], function(text) {
-          console.log(222222)
           console.log(text)
         })
         worker.send({
